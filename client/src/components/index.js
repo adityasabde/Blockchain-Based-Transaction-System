8c672550994d3f6ed305b1d174957f5a7e54dcd1
YOGESH RASAL <94947205+yogesh252002@users.noreply.github.com>
2023-03-16 11:38:10 +0530

    Update index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -4,3 +4,8 @@ export { default as Welcome } from "./Welcome";
 export { default as Footer } from "./Footer";
 export { default as Services } from "./Services";
 export { default as Transactions } from "./Transactions";
+
+
+
+
+
